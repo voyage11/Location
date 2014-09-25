16b4c343de9930824405d9a7cc5081a8f01fc687
Ricky <rickychea11@gmail.com>
2014-09-26 05:49:18 +0800

    Removed unnecessary properties

--- Location/LocationTracker.h
+++ Location/LocationTracker.h
@@ -15,13 +15,6 @@
 @property (nonatomic) CLLocationCoordinate2D myLastLocation;
 @property (nonatomic) CLLocationAccuracy myLastLocationAccuracy;
 
-@property (strong,nonatomic) NSString * accountStatus;
-@property (strong,nonatomic) NSString * authKey;
-@property (strong,nonatomic) NSString * device;
-@property (strong,nonatomic) NSString * name;
-@property (strong,nonatomic) NSString * profilePicURL;
-@property (strong,nonatomic) NSNumber * userid;
-
 @property (strong,nonatomic) LocationShareModel * shareModel;
 
 @property (nonatomic) CLLocationCoordinate2D myLocation;
