68826989c16a66ff2622ccaa7e72ad6ac117865f
Ricky <rickychea11@gmail.com>
2014-10-02 21:09:26 +0800

    Warning when Background App Refresh is Disable
    Added AlertView Warning when Background App Refresh is Disable

--- Location/LocationAppDelegate.m
+++ Location/LocationAppDelegate.m
@@ -13,18 +13,42 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     
-    self.locationTracker = [[LocationTracker alloc]init];
-    [self.locationTracker startLocationTracking];
+     UIAlertView * alert;
     
-    //Send the best location to server every 60 seconds
-    //You may adjust the time interval depends on the need of your app.
-    NSTimeInterval time = 60.0;
-	self.locationUpdateTimer =
-    [NSTimer scheduledTimerWithTimeInterval:time
-                                     target:self
-                                   selector:@selector(updateLocation)
-                                   userInfo:nil
-                                    repeats:YES];
+    //We have to make sure that the Background App Refresh is enable for the Location updates to work in the background.
+    if([[UIApplication sharedApplication] backgroundRefreshStatus] == UIBackgroundRefreshStatusDenied){
+        
+        alert = [[UIAlertView alloc]initWithTitle:@""
+                                          message:@"The app doesn't work without the Background App Refresh enabled. To turn it on, go to Settings > General > Background App Refresh"
+                                         delegate:nil
+                                cancelButtonTitle:@"Ok"
+                                otherButtonTitles:nil, nil];
+        [alert show];
+        
+    }else if([[UIApplication sharedApplication] backgroundRefreshStatus] == UIBackgroundRefreshStatusRestricted){
+        
+        alert = [[UIAlertView alloc]initWithTitle:@""
+                                          message:@"The functions of this app are limited because the Background App Refresh is disable."
+                                         delegate:nil
+                                cancelButtonTitle:@"Ok"
+                                otherButtonTitles:nil, nil];
+        [alert show];
+        
+    } else{
+        
+        self.locationTracker = [[LocationTracker alloc]init];
+        [self.locationTracker startLocationTracking];
+        
+        //Send the best location to server every 60 seconds
+        //You may adjust the time interval depends on the need of your app.
+        NSTimeInterval time = 60.0;
+        self.locationUpdateTimer =
+        [NSTimer scheduledTimerWithTimeInterval:time
+                                         target:self
+                                       selector:@selector(updateLocation)
+                                       userInfo:nil
+                                        repeats:YES];
+    }
     
     return YES;
 }
